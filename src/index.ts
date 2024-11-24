f530371864bab8cb206db9435d1a7fb8eab3e9ec
luisvinicius403 <luisvinicius.dev@gmail.com>
2024-11-24 20:12:51 -0300

    feat: add fs lib to read .txt files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export {};
+import fs from 'fs';
+
+fs.readFile('./src/code.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  console.log(data);
+});
