5836c135aadfb83aa588357f0c958b0be7c4e0fe
luisvinicius403 <luisvinicius.dev@gmail.com>
2024-11-26 23:07:08 -0300

    save

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import chalk from 'chalk';
 import { lexicalAnalyzer } from './analysis/lexical/index.js';
+import { syntaxAnalyzer } from './analysis/syntatic/index.js';
 
 // Read the code from code.txt
 fs.readFile('./src/code.txt', 'utf8', (err, code) => {
@@ -11,10 +12,16 @@ fs.readFile('./src/code.txt', 'utf8', (err, code) => {
 
   // Print the source code
   console.log(`${chalk.magenta('Source Code Read:')}\n${code}\n\n`);
-  
+
   // Run the lexical analysis
   const tokens = lexicalAnalyzer(code);
 
   // Print the tokens with line numbers
   console.log(`${chalk.magenta('Tokens Generated:')}\n`, tokens);
+
+  // Run the syntatic analysis
+  const tree = syntaxAnalyzer(tokens);
+
+  // Print the syntatic tree
+  console.log(`${chalk.magenta('\nSyntatic Tree:')}\n`, tree);
 });
